db7f626f937c6be1be7a1b1a6e90ceb5babd5074
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-17 18:12:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,6 +41,19 @@ attr_accessor :name, :artist, :genre
     @@genre_count
   end 
   
+  def add_student(student_name, grade)
+    @student_name = student_name
+    @grade = grade 
+    if @roster.include?(grade) 
+       @roster[grade] << student_name
+    else  
+     @roster[grade] = []
+     @roster[grade] << student_name
+    end 
+  end
+  
+  
+  
 end 
 
 
