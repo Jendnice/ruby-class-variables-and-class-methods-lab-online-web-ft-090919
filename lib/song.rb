f6b6d4aaf053e30e460a3ede0261f1a15ba0c16f
Jennifer Nicely <pipeline38@hotmail.com>
2019-09-17 18:29:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,7 +42,17 @@ attr_accessor :name, :artist, :genre
     @@genre_count
   end 
   
-  
+   def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1 
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
   
 end 
 
